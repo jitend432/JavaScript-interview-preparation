372a1e059a18fe0ac021ce5b60a795e2e96c0f51
Jitendra Pratap <jitendrapratap432@gmail.com>
2026-04-18 23:26:16 +0530

    fiattenArray

--- CodingPractice/flattenArray.js
+++ CodingPractice/flattenArray.js
@@ -4,3 +4,17 @@ function flattenArray(arr) {
 
 // Example
 console.log(flattenArray([1, [2, [3, 4], 5], 6]));
+
+// --------------------------------------------------------
+
+function flattenArray(arr) {
+  return arr.reduce((acc, curr) => {
+    if (Array.isArray(curr)) {
+      return acc.concat(flattenArray(curr));
+    }
+    return acc.concat(curr);
+  }, []);
+}
+
+// Example
+console.log(flattenArray([1, [2, [3, 4], 5], 6]));
